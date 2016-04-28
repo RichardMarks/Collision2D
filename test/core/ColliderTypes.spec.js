f97ab3f53ddf7a045d3ebc703b212e43dd5f5b88
Richard Marks <ccpsceo@gmail.com>
2016-04-28 15:10:03 -0500

    Added unit test stubs for ColliderTypes

---
+++ test/core/ColliderTypes.spec.js
@@ -0,0 +1,7 @@
+import { TRUE_FALSE } from '../testUtil';
+import { describe, it, expect } from '../imports';
+import { ColliderType, ALPHA_COLLIDER_TYPE, BOX_COLLIDER_TYPE, CIRCLE_COLLIDER_TYPE } from '../../src/core/ColliderTypes';
+
+describe('Collision2D.core.ColliderTypes', () => {
+  it('should provide all supported collider types as constant instances of ColliderType');
+});
