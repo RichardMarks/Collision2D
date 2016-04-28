8854e9e91333aaafc5bd08c7d94cf33180e646d2
Richard Marks <ccpsceo@gmail.com>
2016-04-28 15:10:19 -0500

    Added unit test stubs for ColliderFactory

---
+++ test/core/ColliderFactory.spec.js
@@ -0,0 +1,17 @@
+import { TRUE_FALSE } from '../testUtil';
+import { describe, it, expect } from '../imports';
+import { ColliderFactory } from '../../src/core/ColliderFactory';
+
+describe('Collision2D.core.ColliderFactory', () => {
+  describe('box', () => {
+    it('should construct a new BoxCollider with the supplied parameters');
+  });
+
+  describe('circle', () => {
+    it('should construct a new CircleCollider with the supplied parameters');
+  });
+
+  describe('alpha', () => {
+    it('should construct a new AlphaCollider with the supplied parameters');
+  });
+});
