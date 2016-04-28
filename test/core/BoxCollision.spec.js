9c00500e542763989a7101b27e0af9bdabcd22f4
Richard Marks <ccpsceo@gmail.com>
2016-04-28 15:09:04 -0500

    Added unit test stubs for BoxCollision

---
+++ test/core/BoxCollision.spec.js
@@ -0,0 +1,10 @@
+import { TRUE_FALSE } from '../testUtil';
+import { describe, it, expect } from '../imports';
+import { collidedBox } from '../../src/core/BoxCollision';
+import { ColliderFactory } from '../../src/core/ColliderFactory';
+
+describe('Collision2D.core.BoxCollision', () => {
+  describe('collidedBox', () => {
+    it('should be true when two box colliders collide');
+  });
+});
