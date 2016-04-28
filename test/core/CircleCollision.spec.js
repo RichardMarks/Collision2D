d6100e159fd36d0ca1178810b68739691e308bbc
Richard Marks <ccpsceo@gmail.com>
2016-04-28 15:09:24 -0500

    Added unit test stubs for CircleCollision

---
+++ test/core/CircleCollision.spec.js
@@ -0,0 +1,10 @@
+import { TRUE_FALSE } from '../testUtil';
+import { describe, it, expect } from '../imports';
+import { collidedCircle } from '../../src/core/CircleCollision';
+import { ColliderFactory } from '../../src/core/ColliderFactory';
+
+describe('Collision2D.core.CircleCollision', () => {
+  describe('collidedCircle', () => {
+    it('should be true when two circle colliders collide');
+  });
+});
