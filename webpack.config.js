01f633f3b7cbd317c106c60118e6072d434df67c
Richard Marks <ccpsceo@gmail.com>
2016-06-06 13:14:57 -0500

    Fixed build to output to a static file

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,23 @@
-var pkg = require('./package.json');
 var webpack = require('webpack');
 var path = require('path');
 var env = require('yargs').argv.mode;
 var libraryName = 'Collision2D';
 var plugins = env === 'build' ? [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {screw_ie8: true, keep_fnames: true, warnings: false},
-      mangle: {screw_ie8: true, keep_fnames: true},
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-  ] : [], outputFile;
-
-if (env === 'build') {
-  outputFile = libraryName + '.' + pkg.version + '.min.js';
-} else {
-  outputFile = libraryName + '.js';
-}
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {screw_ie8: true, keep_fnames: true, warnings: false},
+    mangle: {screw_ie8: true, keep_fnames: true},
+  }),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.AggressiveMergingPlugin(),
+] : [];
 
 var config = {
   entry: __dirname + '/src/Collision2D.js',
   devtool: 'source-map',
   output: {
     path: __dirname + '/lib',
-    filename: outputFile,
+    filename: 'c2d.js',
     library: libraryName,
     libraryTarget: 'umd',
     umdNamedDefine: true,
